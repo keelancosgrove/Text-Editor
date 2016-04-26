45dccd31e78e10b55d5f8a56ad6a7439ccb99996
krc78 <krc78@cornell.edu>
2016-04-26 14:53:13 -0400

    Added menu and some comments

--- TextEditor.java
+++ TextEditor.java
@@ -6,6 +6,18 @@ public class TextEditor extends JFrame {
 	
 	//Sets up editor GUI with scrollable pane
 	public TextEditor(){
+		//Instantiates menu items
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem menuItem;
+		
+		menuBar = new JMenuBar();
+		menu = new JMenu("A menu");
+		menuItem = new JMenuItem("Here!!");
+		menu.add(menuItem);
+		menuBar.add(menu);
+		
+		//Creates panel container for text area and save panel
 		JPanel textContainer = new JPanel();
 		textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS));
 		JPanel savePanel = new JPanel();
@@ -13,11 +25,19 @@ public class TextEditor extends JFrame {
 		JTextArea editor = new JTextArea(40,140);
 		JScrollPane textArea = new JScrollPane(editor);
 		setTitle("Text Editor");
+		
+		//Save Panel only contains button for now
 		JButton saveButton = new JButton("save");
 		saveButton.setSize(60,60);
+		
+		//Sets container hierarchy and places elements in JFrame
 		textContainer.add(textArea);
 		savePanel.add(saveButton);
 		textContainer.add(savePanel);
+		add(textContainer);
+		
+		//Sets JFrame properties and makes text editor visible
+		setJMenuBar(menuBar);
 		setSize(200,200);
 		getContentPane().add(BorderLayout.CENTER,textContainer);
 		setLocationByPlatform(true);
@@ -25,9 +45,8 @@ public class TextEditor extends JFrame {
 		setVisible(true);
 	}
 	
-	
-	
 	public static void main(String[] args){
+		//Instantiates new text editor object, with components created from constructor
 		TextEditor editor = new TextEditor();
 	}
 }
